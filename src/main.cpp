d9774d53f211bf6674f9572b289a2cac68df2fe8
Renaud Balu <renaud_balu@hotmail.com>
2026-06-29 18:48:37 +0200

    gestion des boutons et des appuis long

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,43 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
 #include "GpsService.h"
+#include "ButtonService.h"
 
 U8G2_SH1106_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
 
 void setup() {
   Serial.begin(115200);
+
   u8g2.begin();
   u8g2.setFont(u8g2_font_10x20_tr);
+
   initGps();
+
+  buttonSetup();
 }
 
 void loop() {
 
   updateSpeed();
+  ButtonPressed btn = buttonStatus();
+  if (btn == ButtonPressed::LEFT) {
+      Serial.println("LEFT");
+    } else if (btn == ButtonPressed::RIGHT) {
+      Serial.println("RIGHT");
+    } else if (btn == ButtonPressed::BOTH) {
+      Serial.println("BOTH");
+    } else if (btn == ButtonPressed::LEFT_LONG) {
+      Serial.println("LEFT LONG");
+    } else if (btn == ButtonPressed::RIGHT_LONG) {
+      Serial.println("RIGHT LONG");
+    }
 
   u8g2.firstPage();
   do {
     char buffer[20];
     snprintf(buffer, sizeof(buffer), "Vitesse: %d", gpsSpeedKph());
-    u8g2.drawStr(20, 40, buffer);
+    u8g2.drawStr(20, 35, buffer);
+
+    
   } while (u8g2.nextPage());
 }
